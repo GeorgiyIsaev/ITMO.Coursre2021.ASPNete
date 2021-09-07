3ab4c5895a299bcc1cc2da6b2235d5944f01c1ff
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-07 17:31:49 +0300

    add class GuestResponse

---
+++ Lab01.Ex01.EmptyWebSite/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01.Ex01.EmptyWebSite.Models
+{
+    public class GuestResponse
+    {
+        public int GuestResponseIdId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+        public DateTime Rdata { get; set; }
+    }
+}
