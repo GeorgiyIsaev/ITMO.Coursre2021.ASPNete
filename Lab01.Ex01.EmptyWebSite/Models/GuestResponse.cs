c432e8530709bffd2ff539dcceb652375b723f95
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-08 17:33:45 +0300

    add designer to class GuestResponse

--- Lab01.Ex01.EmptyWebSite/Models/GuestResponse.cs
+++ Lab01.Ex01.EmptyWebSite/Models/GuestResponse.cs
@@ -13,5 +13,15 @@ namespace Lab01.Ex01.EmptyWebSite.Models
         public string Phone { get; set; }
         public bool? WillAttend { get; set; }
         public DateTime Rdata { get; set; }
+
+        public GuestResponse() { }
+        public GuestResponse(string name, string email, string phone, bool? willattend) 
+        { 
+            Name = name; 
+            Email = email; 
+            Phone = phone; 
+            WillAttend = willattend; 
+            Rdata = DateTime.Now; 
+        }
     }
 }
