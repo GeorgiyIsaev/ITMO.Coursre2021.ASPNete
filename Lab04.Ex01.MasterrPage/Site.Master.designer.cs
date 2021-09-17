b57ad0581b15728220fbbfb6e8cb9717aed00a37
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-17 14:50:01 +0300

    Размеры

--- Lab04.Ex01.MasterrPage/Site.Master.designer.cs
+++ Lab04.Ex01.MasterrPage/Site.Master.designer.cs
@@ -13,5 +13,23 @@ namespace Lab01.Ex01.EmptyWebSite
 
     public partial class Site
     {
+
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
