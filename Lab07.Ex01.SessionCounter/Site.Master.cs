4a0efc3d60b467567444430da8279ea05e8cf171
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-21 19:22:15 +0300

    Обработчик счетчика

--- Lab07.Ex01.SessionCounter/Site.Master.cs
+++ Lab07.Ex01.SessionCounter/Site.Master.cs
@@ -9,9 +9,13 @@ namespace Lab01.Ex01.EmptyWebSite
 {
     public partial class Site : System.Web.UI.MasterPage
     {
+  
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            long numVisitors = 0;
+            if (Application["Visitors"] != null) { numVisitors = long.Parse(Application["Visitors"].ToString()); }
+            VisitorLiteral.Text = "Число посещений: " + numVisitors.ToString();
         }
     }
+    
 }
