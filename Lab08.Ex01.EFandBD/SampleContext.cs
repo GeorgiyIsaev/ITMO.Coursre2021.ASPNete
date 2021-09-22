69d5f3009ca142652c64e6d9c6cb1f07cde6b52d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-22 20:22:03 +0300

    add class SampleContext

---
+++ Lab08.Ex01.EFandBD/SampleContext.cs
@@ -0,0 +1,16 @@
+﻿using Lab01.Ex01.EmptyWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lab01.Ex01.EmptyWebSite
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("SeminarBD") { }
+        public DbSet<GuestResponse> GuestResponses { get; set; }
+        public DbSet<Report> Reports { get; set; }
+    }
+}
