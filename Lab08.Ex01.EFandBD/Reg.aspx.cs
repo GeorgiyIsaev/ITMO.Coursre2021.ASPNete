d57f16c8af4439044424764f18221406263a090e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-22 21:03:39 +0300

    Запись данных в БД

--- Lab08.Ex01.EFandBD/Reg.aspx.cs
+++ Lab08.Ex01.EFandBD/Reg.aspx.cs
@@ -21,7 +21,27 @@ namespace Lab01.Ex01.EmptyWebSite
                 Page.Validate(); 
                 if (!Page.IsValid) return;
 
-                GuestResponse rsvp = new GuestResponse(name.Text, email.Text, phone.Text, CheckBoxYN.Checked);
+                GuestResponse rsvp = new GuestResponse(name.Text, email.Text, phone.Text, CheckBoxYN.Checked);                
+                if (CheckBoxYN.Checked)
+                {
+                    Report report1 = new Report(TextBoxTitle.Text, TextBoxTextAnnot.Text); 
+                    rsvp.Reports.Add(report1);
+                }
+                if (TextBoxTitle2.Text != "" || TextBoxTextAnnot2.Text != "") 
+                {
+                    Report report2 = new Report(TextBoxTitle2.Text, TextBoxTextAnnot2.Text); 
+                    rsvp.Reports.Add(report2);
+                }
+                try 
+                { 
+                    SampleContext context = new SampleContext();
+                    context.GuestResponses.Add(rsvp);
+                    context.SaveChanges();
+                } 
+                catch (Exception ex) 
+                {
+                    Response.Redirect("Ошибка " + ex.Message); 
+                }
                 ResponseRepository.GetRepository().AddResponse(rsvp);
                 if (rsvp.WillAttend.HasValue && rsvp.WillAttend.Value)
                 {
