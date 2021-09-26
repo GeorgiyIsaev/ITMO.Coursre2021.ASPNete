2995f8a61f27789916bfb0a88869e5437027ed26
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-26 15:45:55 +0300

    add HomeController

---
+++ MVC/Lab01.Ex01.WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Ex01.WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
