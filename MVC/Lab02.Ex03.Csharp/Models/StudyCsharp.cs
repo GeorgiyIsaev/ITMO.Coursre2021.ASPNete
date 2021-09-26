1620c308b02dd436017468f52a7d5a959342673c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-26 16:51:29 +0300

    add class Triangle

--- MVC/Lab02.Ex03.Csharp/Models/StudyCsharp.cs
+++ MVC/Lab02.Ex03.Csharp/Models/StudyCsharp.cs
@@ -98,5 +98,37 @@ namespace Lab01.Ex02.WebMVCR1.Models
             answer = f;
             return ok;
         }
+
+    }
+    public class Triangle
+    {
+        public double Sta { get; set; }
+        public double Stb { get; set; }
+        public double Stc { get; set; }
+        public string Name
+        { 
+            get 
+            { 
+                return String.Format("\"Треугольник со сторонами {0}, {1} и {2}\"",
+                    Sta, Stb, Stc); 
+            } 
+        }
+        public double Perimeter 
+        { 
+            get 
+            { 
+                double p = Sta + Stb + Stc; 
+                return p;
+            } 
+        }
+        public double Area
+        {
+            get 
+            {
+                double sq = Math.Sqrt(Perimeter / 2 * (Perimeter / 2 - Sta)
+                    * (Perimeter / 2 - Stb) * (Perimeter / 2 - Stc)); 
+                return sq; 
+            } 
+        }
     }
 }
