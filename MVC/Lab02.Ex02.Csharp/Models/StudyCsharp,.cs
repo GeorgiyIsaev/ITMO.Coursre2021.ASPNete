5031cb1e04104bc043954e5d5484729fa918e775
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-26 16:27:05 +0300

    create lab02 ex02

---
+++ MVC/Lab02.Ex02.Csharp/Models/StudyCsharp,.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01.Ex02.WebMVCR1.Models
+{
+    public enum AccountType 
+    {
+        Checking,
+        Deposit
+    }
+
+    public struct BankAccount 
+    { 
+        public long accNo;
+        public decimal accBal; 
+        public AccountType accType;
+        public override string ToString() 
+        { 
+            //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}",
+            //    accNo, accBal, accType);
+            string str = $"Номер счета {accNo}, баланс {accBal}, тип {accType}";
+            return str; 
+        }
+    }
+
+
+    public class StudyCsharp_
+    {
+    }
+}
