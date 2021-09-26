a3d892b023a55b02f42ab47114b29b4ec01e84ba
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-26 16:21:52 +0300

    add ExeStruct()

--- MVC/Lab02.Ex01.Csharp/Controllers/HomeController.cs
+++ MVC/Lab02.Ex01.Csharp/Controllers/HomeController.cs
@@ -29,6 +29,19 @@ namespace Lab01.Ex02.WebMVCR1.Controllers
                 platinumAccount);
             string res = res1 + "<p>" + res2;
             return res;
+           
+        } 
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+            string res = String.Format("Номер счета {0}, баланс {1}, тип {2}",
+                goldBankAccount.accNo, goldBankAccount.accBal,
+                goldBankAccount.accType);
+            return res;
         }
-    }
+    } 
 }
