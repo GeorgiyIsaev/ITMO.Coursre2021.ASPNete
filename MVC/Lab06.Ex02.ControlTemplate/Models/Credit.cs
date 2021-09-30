dc1581aa29e15bb7746872edacea5b77b20c6473
Георгий Исаев <georgiyelbaf@gmail.com>
2021-09-30 22:55:46 +0300

    fix credit

--- MVC/Lab06.Ex02.ControlTemplate/Models/Credit.cs
+++ MVC/Lab06.Ex02.ControlTemplate/Models/Credit.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +12,24 @@ namespace Lab05.Ex01.MvcCreditApp1.Models
         // ID кредита
         public virtual int CreditId { get; set; }
         // Название
-        public virtual string Head { get; set; } 
+        [DisplayName("Заголовок")]
+        [Required]
+        public virtual string Head { get; set; }
         // Период, на который выдается кредит 
+        [DisplayName("Период")]
+        [Required]
         public virtual int Period { get; set; }
-        // Максимальная сумма кредита 
+        // Максимальная сумма кредита    
+        [DisplayName("Максимальная сумма")]
+        [Required]
         public virtual int Sum { get; set; }
         // Процентная ставка 
-        public virtual int Procent { get; set; } 
+        [DisplayName("Процент")]
+        [Required]
+        public virtual int Procent { get; set; }
+
+
+     
+
     }
 }
