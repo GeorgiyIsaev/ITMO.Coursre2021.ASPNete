77f69e8d6f453ad04c064eed1f40d2015dc380c1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-10-03 19:57:28 +0300

    add  class Student

---
+++ Akt03.EXAM/ASPNet.EXAM/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNet.EXAM.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public double AverageMark { get; set; } //средний бал
+        public Student() { }
+        public Student(string name, string surname, string group, double averageMark)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            AverageMark = averageMark;
+        }
+
+    }
+}
